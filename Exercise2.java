02c0652ec3b255f774ccf92b683142471c8bc64f
andrewwilliamp <84424039+andrewwilliamp@users.noreply.github.com>
2023-09-30 22:08:20 -0600

    Create Exercise2.java

---
+++ Exercise2.java
@@ -0,0 +1,8 @@
+
+
+
+/*
+Create a file that contains your favorite movie quote. Use a text editor, such as Notepad, and save the file as Quote.txt.
+Copy the file contents and paste them into a word processing program, such as Word. Save the file as Quote.doc. Write an application that displays 
+the sizes of the two files as well as the ratio of the two file sizes. Save the file as FileStatistics2.java.
+/*
